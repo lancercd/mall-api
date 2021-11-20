37a637bdb4aee0820e22013569fc402641bef308
lancercd <1311282756@qq.com>
2021-11-20 16:26:16 +0800

    测试service

---
+++ mall-db/src/test/java/com/mall/db/service/CategoryServiceTest.java
@@ -0,0 +1,22 @@
+package com.mall.db.service;
+
+import com.mall.db.domain.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Test
+    public void findById() {
+        Category byId = categoryService.findById(1);
+        System.out.println(byId);
+
+    }
+}
