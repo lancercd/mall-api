05a95ccb53a152f5ca1b5e7fc0da4ab7e397cbc9
lancercd <1311282756@qq.com>
2021-11-22 23:55:25 +0800

    test CategoryServiceTest

--- mall-db/src/test/java/com/mall/db/service/CategoryServiceTest.java
+++ mall-db/src/test/java/com/mall/db/service/CategoryServiceTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class CategoryServiceTest {
 
     @Autowired
-    private CategoryService categoryService;
+    private CategoryBaseService categoryService;
 
     @Test
     public void findById() {
