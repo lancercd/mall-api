01128f642c5c9815121005dc4e18c17c80f783b3
lancercd <1311282756@qq.com>
2021-11-27 18:37:58 +0800

    添加价格验证  分类验证

--- mall-core/src/main/java/com/mall/core/dto/GoodsDTO.java
+++ mall-core/src/main/java/com/mall/core/dto/GoodsDTO.java
@@ -7,9 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
+import javax.validation.groups.Default;
 import java.math.BigDecimal;
 
 @Data
@@ -17,24 +16,29 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class GoodsDTO {
 
-    @NotNull(message = "id不能为空", groups = Add.class)
+    @NotNull(message = "id不能为空", groups = Update.class)
     private Integer id;
 
     @NotBlank(message = "商品名称不能为空!")
-    @Length(min = 2, message = "商品名称不能少于{min}个字符")
-    @Length(max = 8, message = "商品名称不能对于{max}个字符")
+    @Length(min = 2, message = "商品名称不能少于{min}个字符!")
+    @Length(max = 8, message = "商品名称不能对于{max}个字符!")
     private String name;
 
     @NotBlank(message="商品描述不能为空!")
     private String description;
 
+    @NotNull(message = "请输入商品价格!")
+    @DecimalMax(value = "200000", message = "原价不能大于200000元!")
+    @DecimalMin(value = "1", message = "原价不能小于1元!")
+    @Digits(integer = 6, fraction = 2, message = "小数最多两位!")
     private BigDecimal price;
 
+    @NotNull(message = "请选择商品分类!")
     private Integer categoryId;
 
     @Size(min = 1, message = "至少上传{min}张图片!")
     @Size(max = 2, message = "至多可上传{max}张图片!")
-    @JsonDeserialize(using = StringArrayJsonSerializer.class)
+    // @JsonDeserialize(using = StringArrayJsonSerializer.class)
     private String[] images;
 
     private Boolean status;
